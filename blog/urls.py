7f8f10ebeae726486c4783b7843026e5a75b790a
mano <anshulshongi04@gmail.com>
2022-03-28 22:33:51 +0530

    page 2 made

--- blog/urls.py
+++ blog/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 from turtle import home
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 
 urlpatterns = [
-    path('hacker/', admin.site.urls),
-    path('',include("home.urls")),
-    path('blog/',include("blog.urls")),
     
+    path('',views.index)
 ]
